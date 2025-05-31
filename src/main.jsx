afe7d211addb771355af5d3a404271667dfb8ac9
Dasha-Vin <dashavinaeva98gmail.com@mail.ru>
2025-06-01 01:33:55 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './App.jsx'
 import './index.css'
 
-const root = document.getElementById('root')
-
-createRoot(root).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
